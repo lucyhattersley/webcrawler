e1e2b69a397df24eb1611a004d2e303e8017367e
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-02-21 13:13:06 +0000

    currently have a logic error. keeps adding start url to pages_to_track list

--- webcrawler.py
+++ webcrawler.py
@@ -3,82 +3,110 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-def scan_for_links(soup, webpage):
-    """
-    Takes a soup object (containing a web page response) and webpage, a string of the site's URL.
-    Finds all  links and adds new links to all_links.txt file
-    Returns add_to_tracklist, a list of internal links from the site (strings) to be added to pages_to_track
-    """
-    current_page_links = []
-    add_to_tracklist = []
 
-    # Finds all links in page and appends to current_page_links list
-    for link in soup.find_all('a'):
-        linkstring = (str(link.get('href'))) #typecast to string
-        if linkstring != '' and linkstring[0] == '#': #removes anchors
-            pass
-        elif linkstring != '' and linkstring [0] == '/': # Makes sure link contains main URL
-            newlink = webpage + linkstring
-        else:
-            newlink = linkstring
-        
-        if newlink not in current_page_links:
-            current_page_links.append(newlink) 
-
-    # NOTE: SPLIT THIS OUT INTO SEP FUNCTION
-    # Write all links to file
+def write_to_file(page_links):
+    """
+    Accepts page_links (a list)
+    Opens the all_links.txt file (in amend mode)
+    Iterates through page_links and checks to see if the link is not already in the file (if not, writes the link to the end of the file)
+    Does not return anything
+    """
     all_links = open('all_links.txt', "a")
-    for link in current_page_links:
+    for link in page_links:
         if link not in open('all_links.txt').read(): #prevents duplicates 
             all_links.write(link)
             all_links.write('\n')
     # Close links file 
     all_links.close()
 
-    # Creates list of internal URLs
-    for link in current_page_links:
+    return
+
+def scan_for_links(soup):
+    """
+    Takes a soup object (containing a web page response).
+    Finds all  links and adds to page_links (a list)
+    Returns page_links
+    """
+    page_links = []
+
+    # Finds all links in page
+    # THIS IS CLUNKY CONSIER PUTTING ANCHORS AND RELATIVE URLS INTO SEPARATE FUNCTION
+    for link in soup.find_all('a'):
+        linkstring = (str(link.get('href'))) #typecast to string
+        if linkstring != '' and linkstring[0] == '#': # skips anchors
+            pass
+        elif linkstring != '' and linkstring [0] == '/': # Checks for relative URLs
+            if webpage[-1:] == '/':
+                newlink = webpage[:-1] + linkstring # If relative URL ends in a '/' - removes it so you don't get '//' in newlink
+            else:
+                newlink = webpage + linkstring # places base webpage in front of relative URL to form a full link
+        else:
+            newlink = linkstring
+        if newlink not in page_links:
+            page_links.append(newlink)
+
+    return page_links
+
+def find_internal_links(page_links, webpage):
+    """
+    Accepts page_links (a list) containing URLs and a single webpage
+    Iterates through page links and amends links that match the webpage to internal links (a list)
+    Returns internal_links
+    """
+    internal_links = []
+
+    for link in page_links:
         if link[:len(webpage)] == webpage:
-            add_to_tracklist.append(link) 
+            internal_links.append(link) 
+    return internal_links
 
-    return add_to_tracklist
+def get_soup(webpage):
+    """
+    Accepts webpage (a string) containing a URL
+    Uses urllib2 to generate a request, then a respons
+    Creates a soup object from the response using html.parser
+    Returns soup (a Beautiful Soup object)
+    """
+    request = urllib2.Request(webpage)
+    response = urllib2.urlopen(request)
+    soup = BeautifulSoup(response, "html.parser")
+    return soup
 
+# This has an error. It keeps adding start URL to pagestracked list
 # Start page (this will be added to command line eventually)
-start = 'http://news.bbc.co.uk'
+start = 'http://www.ladywelltavern.com'
 
 #set up tracking lists
 pages_to_track = []
-pages_to_track.append(start)
+webpage = start
+pages_to_track.append(webpage)
 pages_tracked = []
 
-# This needs tidying up and better comments. Maybe split something out into function
-webpage = start
-count = 0 # visual count
+count = 0 # visual count of pages tracked (is displayed to console)
 
-while len(pages_tracked) < 10 or len(pages_to_track) == 0:
+while len(pages_tracked) < 5 or len(pages_to_track) != 0:
 
-    request = urllib2.Request(webpage)
+    if webpage not in pages_tracked:
+        pages_tracked.append(webpage) # keeps track of pages
 
-    # Gets content from web page and passes it into a soup object
+    webpage = pages_to_track.pop(0)
+
     try:
-        response = urllib2.urlopen(request)
+        soup = get_soup(webpage)
+        page_links = scan_for_links(soup)
+        write_to_file(page_links)
+
+        new_pages_found = find_internal_links(page_links, webpage)
+
+        for page in new_pages_found:
+            if page not in pages_to_track:
+                pages_to_track.append(page)
+                with open('pages_to_track.txt', 'a') as f:
+                    f.write(page)
+                    f.write('\n')
+
+        count += 1
+        print "Number of pages tracked: " + str(count)
+
     except:
         pass # skips pages that don't respond
-    soup = BeautifulSoup(response, "html.parser")
-
-    new_pages_found = scan_for_links(soup, webpage)
- 
-    for page in new_pages_found:
-        if page not in pages_to_track:
-            pages_to_track.append(page)
-    
-    if page not in pages_tracked:
-        pages_tracked.append(webpage) # keeps track of pages
- 
-    try:
-        webpage = pages_to_track.pop(0)
-    except:
-        print "No pages left to track"
-        break
-    
-    count += 1
-    print "Number of pages tracked: " + str(count)
