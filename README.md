fa498e279447bca742d9f644412f29d458356c56
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-02-21 10:45:42 +0000

    Updated README.md and removed old html files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# webcrawler
+Webcrawler scans webpages and extracts links. It then uses these to find other (internal) pages of a website and saves all links it finds to a text file. It is used to create a list of all the webpages in a website. Further iterations will also collect information on each page, such as number of outbound links, inbound links, title, keywords etc. It is hoped that eventually it will be smart enough to gather detailed information about which are the most important internal pages in a website.
+
+For now it just scans a website and creates a list of webpages. 
+
+Webcrawler uses BeautifulSoup 4. Install with "pip install beautifulsoup4" first.
+
+lucy.a.hattersley@gmail.com
--- webcrawler.py
+++ webcrawler.py
@@ -1,32 +1,84 @@
-# This creates a 'links.txt' file containing all html links in a webpage
-# NEXT STEPS: Go through links and find valid links.
-# Open a child link as a second soup
+# This creates a 'links.txt' file containing all html links found in website
 
-
-# References:
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#kinds-of-objects
-# http://stackoverflow.com/questions/1843422/get-webpage-contents-with-python
 from bs4 import BeautifulSoup
 import urllib2
 
-# creates output file in same directory
-f = open('links.txt', "w+")
+def scan_for_links(soup, webpage):
+    """
+    Takes a soup object (containing a web page response) and webpage, a string of the site's URL.
+    Finds all  links and adds new links to all_links.txt file
+    Returns add_to_tracklist, a list of internal links from the site (strings) to be added to pages_to_track
+    """
+    current_page_links = []
+    add_to_tracklist = []
 
-# target webpage
-request = urllib2.Request('http://news.bbc.co.uk')
-links = []
+    # Finds all links in page and appends to current_page_links list
+    for link in soup.find_all('a'):
+        linkstring = (str(link.get('href'))) #typecast to string
+        if linkstring != '' and linkstring[0] == '#': #removes anchors
+            pass
+        elif linkstring != '' and linkstring [0] == '/': # Makes sure link contains main URL
+            newlink = webpage + linkstring
+        else:
+            newlink = linkstring
+        
+        if newlink not in current_page_links:
+            current_page_links.append(newlink) 
 
-# Gets content from web page and passes it into a soup object
-response = urllib2.urlopen(request)
-soup = BeautifulSoup(response)
+    # NOTE: SPLIT THIS OUT INTO SEP FUNCTION
+    # Write all links to file
+    all_links = open('all_links.txt', "a")
+    for link in current_page_links:
+        if link not in open('all_links.txt').read(): #prevents duplicates 
+            all_links.write(link)
+            all_links.write('\n')
+    # Close links file 
+    all_links.close()
 
-# Finds all links in page and appends to links list
-for link in soup.find_all('a'):
-    links.append(link.get('href'))
-#    print(link.get('href')) # used to test links
+    # Creates list of internal URLs
+    for link in current_page_links:
+        if link[:len(webpage)] == webpage:
+            add_to_tracklist.append(link) 
 
-# Writes each link to file
-for link in links:
-	f.write(link)
-	f.write('\n')
-f.close()
+    return add_to_tracklist
+
+# Start page (this will be added to command line eventually)
+start = 'http://news.bbc.co.uk'
+
+#set up tracking lists
+pages_to_track = []
+pages_to_track.append(start)
+pages_tracked = []
+
+# This needs tidying up and better comments. Maybe split something out into function
+webpage = start
+count = 0 # visual count
+
+while len(pages_tracked) < 10 or len(pages_to_track) == 0:
+
+    request = urllib2.Request(webpage)
+
+    # Gets content from web page and passes it into a soup object
+    try:
+        response = urllib2.urlopen(request)
+    except:
+        pass # skips pages that don't respond
+    soup = BeautifulSoup(response, "html.parser")
+
+    new_pages_found = scan_for_links(soup, webpage)
+ 
+    for page in new_pages_found:
+        if page not in pages_to_track:
+            pages_to_track.append(page)
+    
+    if page not in pages_tracked:
+        pages_tracked.append(webpage) # keeps track of pages
+ 
+    try:
+        webpage = pages_to_track.pop(0)
+    except:
+        print "No pages left to track"
+        break
+    
+    count += 1
+    print "Number of pages tracked: " + str(count)
