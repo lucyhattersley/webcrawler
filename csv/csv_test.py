5171174fba93aa92b296a122b087281f4ff3637d
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-03-04 10:06:17 +0000

    working through Python for Beginners CSV

--- csv/csv_test.py
+++ csv/csv_test.py
@@ -1,5 +1,21 @@
+#http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/
+
+#To be able to read csv formatted files, we will first have to import the
+#csv module.
+# import csv
+# with open('eggs.csv','rb') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print row
+
+#Example2
 import csv
-with open('eggs.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	for row in spamreader:
-		print ', '.join(row)
+import sys
+
+f=open(sys.argv[1],'rU') #opens the csv file
+try:
+    reader = csv.reader(f) # creates the reader object
+    for row in reader: # iterates the rows of the file in orders
+        print row
+finally:
+    f.close()    #closing
